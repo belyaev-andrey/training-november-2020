4a07b7ae385b0b37c056134977e4c9cd1bab44c7
Andrey Belyaev <belyaev.andrey@gmail.com>
2020-11-30 15:02:19 +0300

    Added used consumables screen

---
+++ modules/web/src/com/company/clinic/web/screens/consumable/UsedConsumables.java
@@ -0,0 +1,25 @@
+package com.company.clinic.web.screens.consumable;
+
+import com.company.clinic.entity.Consumable;
+import com.company.clinic.service.ConsumablesService;
+import com.haulmont.cuba.core.global.LoadContext;
+import com.haulmont.cuba.gui.model.CollectionContainer;
+import com.haulmont.cuba.gui.model.CollectionLoader;
+import com.haulmont.cuba.gui.screen.*;
+
+import javax.inject.Inject;
+import java.util.List;
+
+@UiController("clinic_UsedConsumables")
+@UiDescriptor("used-consumables.xml")
+public class UsedConsumables extends Screen {
+
+    @Inject
+    private ConsumablesService consumablesService;
+
+    @Install(to = "consumablesDl", target = Target.DATA_LOADER)
+    private List<Consumable> consumablesDlLoadDelegate(LoadContext<Consumable> loadContext) {
+        return consumablesService.getUsedConsumables();
+    }
+
+}
